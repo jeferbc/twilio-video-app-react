e28c97d0c8ea972d766501d79d23edf5d3748e78
jeferbc <jeffe.bernal@gmail.com>
2021-08-05 20:20:51 -0500

    remove unused util method

--- src/utils/useAvatar/useAvatar.ts
+++
@@ -1,9 +0,0 @@
-import useParticipants from '../../hooks/useParticipants/useParticipants';
-
-export default function useAvatar(conversation) {
-  const participants = useParticipants();
-  console.log(conversation)
-  console.log(participants)
-
-  return null
-}
