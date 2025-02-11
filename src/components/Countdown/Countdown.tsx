bfb8635be0a3f3ab7a066136134108cc2ea662e6
jeferbc <jeffe.bernal@gmail.com>
2025-02-11 11:35:41 -0500

    Add messages to the user

--- src/components/Countdown/Countdown.tsx
+++ src/components/Countdown/Countdown.tsx
@@ -1,41 +1,99 @@
-import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import React, { useEffect, useMemo, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
 import useCountdown from '../../hooks/useCountdown/useCountdown'
 import { useAppState } from '../../state';
+import useVideoContext from '../../hooks/useVideoContext/useVideoContext';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    container: {
-      marginLeft: '1em',
-      fontSize: '1.3em',
-    },
-  })
-);
+const useStyles = makeStyles(() => ({
+  container: {
+    marginLeft: '1em',
+    fontSize: '1em',
+  },
+  link: {
+    color: 'rgb(40, 42, 43)',
+    fontWeight: 'bold',
+  },
+}));
 
 export default function Countdown() {
-  const { user } = useAppState();
-  const [showAlert, setShowAlert] = React.useState(true);
-  const timeLeft = useCountdown();
   const classes = useStyles();
+  const { user } = useAppState();
+  const timeLeft = useCountdown();
+  const { room } = useVideoContext();
+  const participants = Array.from(room?.participants.values() ?? []);
 
-  if (timeLeft.minutes < 1 && showAlert) {
-    let text = 'En menos de 1 minuto esta sala se cerrará automáticamente.';
-    text+= user.userType === 'Doctor' ? ' Favor darle cierre a la cita o llamar al usuario y terminarla por otro medio' : 'Gracias por usar nuestros servicios.';
-    window.growl('danger', text);
-    setShowAlert(false);
-  }
+  const [alertShown, setAlertShown] = useState({ finalMinute: false });
+
+  const timeForHelp = 50;
+  const isDoctor = user?.userType === 'Doctor';
+  const noParticipants = participants.length === 0;
+  const noParticipantConnectedText = `Hola, yo ${user?.displayName} estoy en la sala esperando al psicólogo, pero aún no se ha conectado.`;
+
+  const roomStatus = useMemo(() => {
+    const participantConnected = sessionStorage.getItem('participantConnected');
+    if (noParticipants) {
+      if (!participantConnected && timeLeft.minutes < timeForHelp) {
+        return isDoctor
+          ? 'Esperando al paciente'
+          : (
+            <a
+              href={`https://wa.me/573184310302?text=${encodeURIComponent(noParticipantConnectedText)}`}
+              target="_blank"
+              className={classes.link}
+              rel="noopener noreferrer"
+            >
+              Contáctanos
+            </a>
+          );
+      }
+      return isDoctor ? 'Esperando al paciente' : 'Esperando al psicólogo';
+    }
+
+    sessionStorage.setItem('participantConnected', 'true');
+    return 'Cita en proceso';
+  }, [noParticipants, timeLeft.minutes]);
+
+  useEffect(() => {
+    if (!alertShown.finalMinute && timeLeft.minutes < 1) {
+      const message = isDoctor
+        ? 'En menos de 1 minuto esta sala se cerrará automáticamente. Favor darle cierre a la cita o llamar al usuario y terminarla por otro medio.'
+        : 'En menos de 1 minuto esta sala se cerrará automáticamente.';
+      
+      window.growl('danger', message);
+      setAlertShown(prev => ({ ...prev, finalMinute: true }));
+    }
+  }, [timeLeft.minutes, alertShown.finalMinute, isDoctor]);
+
+  useEffect(() => {
+    const noParticipantsAlert = sessionStorage.getItem('noParticipantsAlert');
+    if (!noParticipantsAlert && noParticipants && timeLeft.minutes < timeForHelp) {
+      const noParticipantMessage = isDoctor
+        ? 'El paciente aún no ha ingresado a la sala. Por favor, comuníquese con él.'
+        : `<a href='https://wa.me/573184310302?text=${encodeURIComponent(noParticipantConnectedText)}'
+              target='_blank'
+              class='${classes.link}'
+              rel='noopener noreferrer'>
+             El psicólogo aún no se ha conectado. Por favor, solicita ayuda haciendo clic aquí.
+           </a>`;
+
+      window.growl('danger', noParticipantMessage);
+      sessionStorage.setItem('noParticipantsAlert', 'true');
+    }
+  }, [noParticipants, timeLeft.minutes, alertShown.noParticipants, isDoctor, classes.link, noParticipantConnectedText]);
 
   return (
-    // show countdown only for doctors when 10 minutes remaining. User see 'Cerrando cita' when 5
-    // minutes remaining
     <span className={classes.container}>
-      { timeLeft.minutes < 10 && user.userType === 'Doctor' ? (
+      {isDoctor && timeLeft.minutes < 10 ? (
         <span>
-        { timeLeft.minutes < 10 ? `0${ timeLeft.minutes }` : timeLeft.minutes }:{ timeLeft.seconds < 10 ? `0${ timeLeft.seconds }` : timeLeft.seconds }
+          {`${timeLeft.minutes < 10 ? `0${timeLeft.minutes}` : timeLeft.minutes}`}:
+          {`${timeLeft.seconds < 10 ? `0${timeLeft.seconds}` : timeLeft.seconds}`}
         </span>
-      ) : timeLeft.minutes < 5 && user.userType === 'User' ? ( <span>Cerrando cita</span> ) : ( <span>Cita en proceso</span> )
-      }
+      ) : !isDoctor && timeLeft.minutes < 5 ? (
+        <span>Cerrando cita</span>
+      ) : (
+        <span>{roomStatus}</span>
+      )}
     </span>
-  )
+  );
 }
